2563110dccf32f238dd8a9f087af45a08a13fdbd
Julien BALLANGER <jballang@e2r10p11.42.fr>
2018-04-19 15:38:56 +0200

    update header

--- malloc/includes/malloc.h
+++ malloc/includes/malloc.h
@@ -6,31 +6,34 @@
 /*   By: jballang <jballang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/19 11:48:19 by jballang          #+#    #+#             */
-/*   Updated: 2018/04/19 13:22:21 by jballang         ###   ########.fr       */
+/*   Updated: 2018/04/19 15:37:44 by jballang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MALLOC_H
 # define MALLOC_H
-# define BLOCK_SIZE sizeof(struct s_block)
+# define BLOCK_SIZE sizeof(struct s_header)
 
 # include "../libft/libft.h"
 
 # include <sys/mman.h>
 # include <unistd.h>
 
-typedef struct s_block	*t_block;
+typedef struct s_header	t_header;
 
-struct	s_block
+struct		s_header
 {
-	size_t	size;
-	t_block	next;
+	size_t		size;
 	int			free;
+	t_header	*next;
 };
 
-extern void	*base;
-void *base;
+extern t_header	*g_head;
+extern t_header	*g_tail;
+t_header *g_head;
+t_header *g_tail;
 
-t_block	find_block(t_block *last, size_t size);
+void		*ft_malloc(size_t size);
+t_header	*find_block(size_t size);
 
 #endif
